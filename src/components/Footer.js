b5825c81601099427d0ee6dd71a994830adaed66
Anssi Peltola <anssipeltola@hotmail.com>
2025-01-13 11:28:35 +0200

    Footerin vuosi päivittyy automaattisesti

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,11 @@
 import React from "react";
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
     <footer className="bg-ksuPurple text-white p-4 text-center rounded-lg mx-auto w-1/2">
-      <p>&copy; 2024 Anssi Peltola. All rights reserved.</p>
+      <p>&copy; {currentYear} Anssi Peltola. All rights reserved.</p>
     </footer>
   );
 };
